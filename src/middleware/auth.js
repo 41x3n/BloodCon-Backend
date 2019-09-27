00607688bb4ca5b3b63679d4ab01bd4be4fd041b
Anindya Chowdhury <anindya50@live.com>
2019-09-27 18:20:42 +0530

    Create auth.js

---
+++ src/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const Donor = require('../models/donor')
+
+// Authenticating Donor Account
+const dAuth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '')
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const donor = await Donor.findOne({ _id: decoded._id, 'tokens.token': token })
+    if(!donor) {
+      throw new Error()
+    }
+    req.token = token
+    req.donor = donor
+    next()
+  } catch (error) {
+    res.status(401).send({ error: 'Please authenticate.'})
+  }
+}
+
+module.exports = { dAuth }
