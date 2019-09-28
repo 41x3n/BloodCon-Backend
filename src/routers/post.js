9f8310bb59b48c6e89fcf0f8cf4fba4a4a35f51d
Anindya Chowdhury <anindya50@live.com>
2019-09-28 18:38:59 +0530

    setting up post routes

--- src/routers/post.js
+++ src/routers/post.js
@@ -1,7 +1,107 @@
 const express = require('express')
 const Post = require('../models/post')
+const { rAuth } = require('../middleware/auth')
 const router = new express.Router()
 
+// Route to post a post
+router.post('/posts', rAuth, async (req, res) => {
+  const post = new Post({
+    ...req.body,
+    owner: req.receiver._id
+  })
+
+  try {
+    await post.save()
+    res.status(201).send(post)
+  } catch (error) {
+    res.status(200).send(error)
+  }
+})
+
+// Route to get all post on basis of URL queries
+router.get('/postsAll', async (req, res) => {
+  try {
+    const queryList = Object.keys(req.query)
+    console.log(queryList);
+    const list = {}
+    queryList.forEach((query) => {
+      console.log(query);
+      if(query !== 'limit' && query !== 'skip') {
+        list[query] = req.query[query]
+      }
+    })
+    console.log(list);
+    const posts = await Post.find( list ).limit(parseInt(req.query.limit)).sort( '-createdAt' ).skip(parseInt(req.query.skip))
+    res.status(200).send(posts)
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
+// Route to get all posts by the logged in receiver
+router.get('/posts', rAuth, async (req, res) => {
+  try {
+    await req.receiver.populate({
+      path: 'posts'
+    }).execPopulate()
+    res.status(200).send(req.receiver.posts)
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
+// Route to get a post by ID
+router.get('/posts/:id', rAuth, async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const post = await Post.findOne({ _id, owner: req.receiver._id})
+    if(!post) {
+      return res.status(404).send()
+    }
+    res.status(200).send(post)
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
+// Route to update a post
+router.patch('/posts/:id', rAuth, async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['desc', "hospital", "address", "city", "state", "zipCode", "country", "phone", "bloodGroup"]
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if(!isValidOperation) {
+    res.status(400).send({ error: 'Invalid updates!'})
+  }
+
+  try {
+    const post = await Post.findOne({ _id: req.params.id, owner: req.receiver._id })
+
+    if(!post){
+      return res.status(404).send()
+    }
+
+    updates.forEach((update) => post[update] = req.body[update])
+    await post.save()
+    res.status(200).send(post)
+  } catch (error) {
+    res.status(400).send()
+  }
+})
+
+// Route to delete a post
+router.delete("/posts/:id", rAuth, async (req, res) => {
+  try {
+    const post = await Post.findOneAndDelete({ _id: req.params.id, owner: req.receiver._id })
+    if(!post){
+      return res.status(404).send()
+    }
+    res.status(200).send(post)
+  } catch (error) {
+    res.status(400).send()
+  }
+})
 
 
 module.exports = router
