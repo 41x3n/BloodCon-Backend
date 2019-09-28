5599e72895a347f41bef994c3a85c755fc3f1ff3
Anindya Chowdhury <anindya50@live.com>
2019-09-28 18:38:43 +0530

    setting up post routes

--- src/models/post.js
+++ src/models/post.js
@@ -1,7 +1,56 @@
 const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
-
+  desc: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  bloodGroup: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  hospital: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  state: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  zipCode: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Receiver'
+  }
+}, {
+  timestamps: true
 })
 
 const Post = mongoose.model('Post', postSchema)
